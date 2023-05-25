8c9b90efb0fcb5d3c71fbea134f9141d00cac0d2
Mariaeduardarg <msitescriativos@gmail.com>
2023-05-25 18:58:37 -0300

    maria-eduarda

--- index.php
+++ index.php
@@ -1,45 +1,76 @@
-
-
-<?php
-// Aqui eu estou verificando se o usuário digitou algum número
-
-if (isset($_GET['posicao'])) {
-    // Obtém o valor enviado pelo usuário
-    $posicao = $_GET['posicao'];
-
-    // Função para calcular a sequência de Fibonacci
-    function calculo($num)
-    {
-        $fib = [0, 1];
-
-        for ($i = 2; $i <= $num; $i++) {
-            $fib[$i] = bcadd($fib[$i - 1], $fib[$i - 2]);
-        }
-
-        return $fib[$num];
-    }
-
-    // Calcula o número na posição informada
-    $resultado = calculo($posicao);
-}
-?>
-
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Sequência de Fibonacci</title>
 </head>
+
 <body>
-    <h1>Sequência de Fibonacci</h1>
+    <style>
+        .container {
+            width: 15rem;
+            margin-left: 35rem;
+            margin-top: 14rem;
+            padding: 3rem 0rem 3rem 3rem;
+            background: linear-gradient(120deg, #F8CBCC, #EF4BD0);
+            border-radius: 10rem;
+        }
 
-    <form method="get" action="index.php">
-        <label for="posicao">Informe a posição:</label>
-        <input type="number" name="posicao" id="posicao" required>
-        <button type="submit">Calcular</button>
-    </form>
+        input {
+            border-radius: 5rem;
+            border: none;
+            margin-left: 0.5rem;
+        }
 
-    <?php if (isset($resultado)) : ?>
-        <p>O número na posição <?= $posicao ?> da sequência de Fibonacci é: <?= $resultado ?></p>
-    <?php endif; ?>
+        h1 {
+
+            color: white;
+        }
+
+        h2 {
+
+            color: white;
+            font-size: 15px;
+            font-weight: 300;
+            margin-left: 0.5rem;
+        }
+
+        body {
+            background: linear-gradient(90deg, #ffe53bd8, #ff2525da);
+        }
+
+        button {
+            border-radius: 10px;
+            border: none;
+            background-color: palevioletred;
+            color: black;
+            margin-top: 1rem;
+            margin-left: 4rem;
+        }
+    </style>
+
+    <div class="container">
+        <h1>Sequência de Fibonacci</h1>
+        <form action="" method="get">
+            <h2>Coloque aqui um número:</h2>
+            <input type="number" name="posicao" id="posicao">
+            <br>
+            <button>Calcular</button>
+        </form>
+        <?php
+        if (isset($_GET['posicao'])) {
+            $number = $_GET['posicao'];
+            $lastNumber = 1;
+            $penultimateNumber = 0;
+            for ($i = 1; $i <= $number - 2; $i++) {
+                $num = $lastNumber + $penultimateNumber;
+                echo $num . "<br>";
+                $penultimateNumber = $lastNumber;
+                $lastNumber = $num;
+            }
+        }
+        ?>
+    </div>
 </body>
+
 </html>
