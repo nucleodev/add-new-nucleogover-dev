3e9a0264a080559cc1e5a83e1334cf29ff197b25
Yago Prado <yagojprado@gmail.com>
2023-05-26 08:52:11 -0300

    Adição no index.php de get footer e header e criação dos mesmos.

--- wordpress/wp-content/themes/nucleogov/index.php
+++ wordpress/wp-content/themes/nucleogov/index.php
@@ -1,9 +1,11 @@
 <?php
 
+get_header();
+
 if ( have_posts() ) :
     while ( have_posts() ) : the_post(); ?>
 
-        <h2><?php the_title() ?></h2>
+        <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
         <?php the_content() ?>
 
     <?php endwhile;
@@ -12,5 +14,7 @@ else :
     echo '<p>There are no posts!</p>';
 
 endif;
+
+get_footer();
 
 ?>
