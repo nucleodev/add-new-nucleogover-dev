dde0d2f23cc1d7dd86a822ec24a64bd6b66e6f5c
Yago Prado <yagojprado@gmail.com>
2023-05-31 15:57:21 -0300

    Adição do post-title do block da noticia

--- wordpress/wp-content/themes/nucleogov/index.php
+++ wordpress/wp-content/themes/nucleogov/index.php
@@ -2,32 +2,41 @@
 
 get_header();
 ?>
-    <main class="site-main">
-        <div class="container">
-            <div class="posts">
-                <div class="post-title">
-                    <h1>ÚLTIMAS NOTÍCIAS</h1>
-                    <p><a href="#">VER TODAS AS NOTÍCIAS</a></p>
-                </div>
-                <div class="post-list">
-                    <?php
-                    if (have_posts()) :
-                        while (have_posts()) : the_post(); ?>
-                            <article>
-                                <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
-                                <?php the_content() ?>
-                            </article>
-                        <?php endwhile;
-
-                    else :
-                        echo '<p>There are no posts!</p>';
-
-                    endif;
-                    ?>
-                </div>
+<main class="site-main">
+    <div class="container">
+        <div class="posts">
+            <div class="post-title">
+                <h1>ÚLTIMAS NOTÍCIAS</h1>
+                <a href="" class="last-news">
+                    <p>
+                        VER TODAS AS NOTÍCIAS
+                    </p>
+                    <svg viewBox="-2.4 -2.4 28.80 28.80" xmlns="http://www.w3.org/2000/svg" fill="#69C549"
+                         transform="matrix(-1, 0, 0, 1, 0, 0)"
+                         style="vertical-align: middle; margin-left: 5px;">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <title></title>
+                            <g id="Complete">
+                                <g id="browsers">
+                                    <g>
+                                        <rect fill="none" height="14" rx="2" ry="2" stroke="#69C549"
+                                              stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                              width="14" x="3" y="7"></rect>
+                                        <path d="M8,3H19a2,2,0,0,1,2,2V16" fill="none" stroke="#69C549"
+                                              stroke-linecap="round" stroke-linejoin="round"
+                                              stroke-width="2"></path>
+                                    </g>
+                                </g>
+                            </g>
+                        </g>
+                    </svg>
+                </a>
             </div>
         </div>
-    </main>
+    </div>
+</main>
 <?php
 get_footer();
 ?>
