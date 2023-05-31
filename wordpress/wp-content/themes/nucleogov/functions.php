d6c69b66dc1c8859a6c1735fe92cfea56248c072
Yago Prado <yagojprado@gmail.com>
2023-05-31 17:17:15 -0300

    Alteração no admin_menu para adicionar o MENU de Noticias.

--- wordpress/wp-content/themes/nucleogov/functions.php
+++ wordpress/wp-content/themes/nucleogov/functions.php
@@ -1,5 +1,25 @@
 <?php
-function custom_theme_assets() {
+function custom_theme_assets() {//adição do style nos arquivos do php
     wp_enqueue_style( 'style', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'custom_theme_assets' );
+
+function custom_menu_noticias() {//adição do menu de Noticias no menu do admin
+    add_menu_page(
+        'Adicionar Notícias', // titulo
+        'Notícias', // menu
+        'manage_options', // capacidade de usuário necessária para acessar o menu
+        'editar-noticias', // slug
+        'noticias_callback', // callback para exibir o menu
+        'dashicons-welcome-write-blog', // icone
+        2 // posicao
+    );
+}
+add_action( 'admin_menu', 'custom_menu_noticias' );
+
+function noticias_callback() {
+    echo '<h1>Meu Menu Personalizado</h1>';
+    echo '<p>Este é o conteúdo do meu menu personalizado.</p>';
+}
+
+
--- wordpress/wp-content/themes/nucleogov/header.php
+++ wordpress/wp-content/themes/nucleogov/header.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title><?php bloginfo('name'); ?></title>
     <?php wp_head() ?>
 </head>
