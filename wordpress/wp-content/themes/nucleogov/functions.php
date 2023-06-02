407978ee420b2cff88d768541f93b8b66c4aa4d5
Yago Prado <yagojprado@gmail.com>
2023-06-02 16:17:11 -0300

    alteração do menu "POST" do painel admin, para Notícias, tanto de nome quanto de icone

--- wordpress/wp-content/themes/nucleogov/functions.php
+++ wordpress/wp-content/themes/nucleogov/functions.php
@@ -1,25 +1,19 @@
 <?php
-function custom_theme_assets() {//adição do style nos arquivos do php
+//adição do style nos arquivos do php
+function custom_theme_assets() {
     wp_enqueue_style( 'style', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'custom_theme_assets' );
 
-function custom_menu_noticias() {//adição do menu de Noticias no menu do admin
-    add_menu_page(
-        'Adicionar Notícias', // titulo
-        'Notícias', // menu
-        'manage_options', // capacidade de usuário necessária para acessar o menu
-        'editar-noticias', // slug
-        'noticias_callback', // callback para exibir o menu
-        'dashicons-welcome-write-blog', // icone
-        2 // posicao
-    );
-}
-add_action( 'admin_menu', 'custom_menu_noticias' );
 
-function noticias_callback() {
-    echo '<h1>Meu Menu Personalizado</h1>';
-    echo '<p>Este é o conteúdo do meu menu personalizado.</p>';
+//adiciona suporte para adicionar imagem de destaque no post
+add_theme_support( 'post-thumbnails' );
+
+//alteração do menu "POST" do painel admin
+function changeMenu() {
+    global $menu;
+    $menu[5][0] = 'Notícias';
+    $menu[5][6] = 'dashicons-welcome-write-blog';
 }
 
-
+add_action('admin_menu', 'changeMenu');
