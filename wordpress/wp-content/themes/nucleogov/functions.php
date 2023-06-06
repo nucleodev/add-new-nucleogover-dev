f870522283fc8f39e0be5b8e78d7ba70a29da741
Yago Prado <yagojprado@gmail.com>
2023-06-06 16:04:40 -0300

    Adição dos arquivos header e footer, e style.css para ficar de acordo com o modelo

---
+++ wordpress/wp-content/themes/nucleogov/functions.php
@@ -0,0 +1,6 @@
+<?php
+//adição do style nos arquivos do php
+function custom_theme_assets() {
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'custom_theme_assets' );
---
+++ wordpress/wp-content/themes/nucleogov/index.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Template Name: Página com Blocos Padrão
+*/
+
+get_header();
+?>
+<main>
+    <div class="container">
+<?php
+while (have_posts()) {
+    the_post();
+
+    // Exibe os blocos da página definida como modelo padrão
+    if (function_exists('render_block')) {
+        $content_blocks = parse_blocks(get_post_field('post_content', get_option('page_on_front')));
+        foreach ($content_blocks as $block) {
+            echo render_block($block);
+        }
+    }
+}
+?>
+    </div>
+</main>
+<?php
+get_footer();
+?>
