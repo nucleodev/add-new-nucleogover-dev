e37eede2e1065dcc0c5a52cfcdc5729907d910e6
Yago Prado <yagojprado@gmail.com>
2023-06-07 16:47:12 -0300

    Adição do bloco de noticias no admin menu do wordpress

--- wordpress/wp-content/themes/nucleogov/functions.php
+++ wordpress/wp-content/themes/nucleogov/functions.php
@@ -1,8 +1,22 @@
 <?php
 //adição do style nos arquivos do php
 function custom_theme_assets() {
-	wp_enqueue_style( 'style', get_template_directory_uri() . '/css/style.css');
+	$css_dir = get_stylesheet_directory_uri() . '/css';
+
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
 	wp_enqueue_style( 'header', get_template_directory_uri() . '/css/header.css');
 	wp_enqueue_style( 'footer', get_template_directory_uri() . '/css/footer.css');
+
+	wp_enqueue_style('bloco-noticia', $css_dir . '/bloco-noticia.css', []);
 }
 add_action( 'wp_enqueue_scripts', 'custom_theme_assets' );
+
+
+function my_enqueue_block_assets() {
+	$css_dir = get_stylesheet_directory_uri() . '/css';
+	$js_dir = get_stylesheet_directory_uri() . '/js';
+
+	wp_enqueue_script('bloco-noticia', $js_dir . '/bloco-noticia.js', [ 'wp-blocks', 'wp-dom' ] , null, true);
+	wp_enqueue_style('bloco-noticia', $css_dir . '/bloco-noticia.css', [ 'wp-edit-blocks' ]);
+}
+add_action('enqueue_block_editor_assets', 'my_enqueue_block_assets');
---
+++ wordpress/wp-content/themes/nucleogov/js/bloco-noticia.js
@@ -0,0 +1,46 @@
+(() => {
+    const el = window.wp.element.createElement;
+    const { registerBlockType } = window.wp.blocks;
+    const { RichText } = window.wp.blockEditor;
+
+    registerBlockType('nucleogov/news', {
+        title: 'Noticias',
+        icon: 'welcome-write-blog',
+        category: 'layout',
+        attributes: {
+            content: {
+                type: 'array',
+                source: 'children',
+                selector: 'p',
+            },
+        },
+        edit: myEdit,
+        save: mySave
+    });
+
+    // what's rendered in admin
+    function myEdit(props) {
+        const atts = props.attributes;
+
+        return el(RichText, {
+            tagName: 'p',
+            className: props.className,
+            value: atts.content,
+
+            // Listener when the RichText is changed.
+            onChange: (value) => {
+                props.setAttributes({ content: value });
+            },
+        });
+    }
+
+    // what's saved in database and rendered in frontend
+    function mySave(props) {
+        const atts = props.attributes;
+
+        return el(RichText.Content, {
+            tagName: 'p',
+            value: atts.content,
+        });
+    }
+})();
